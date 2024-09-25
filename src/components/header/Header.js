d4550f54726337da0c6457566b4e272e62f844af
Davi Augusto <davi_augusto11@hotmail.com>
2024-09-25 01:54:47 -0300

    feat: ajustar bordas em baixo dos itens do menu e barra de rolagem horizontal da página

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,78 +1,87 @@
-import React from 'react'
-import Style from '../header/Style.header.css'
-import Carneiros from '../img/carneiros.jpg'
-import Porto from '../img/porto.jpg'
-import Noronha from '../img/praia2.jpg'
-import Enseada from '../img/enseada-dos-corais.jpg'
+import React from "react";
+import Style from "../header/Style.header.css";
+import Carneiros from "../img/carneiros.jpg";
+import Porto from "../img/porto.jpg";
+import Noronha from "../img/praia2.jpg";
+import Enseada from "../img/enseada-dos-corais.jpg";
+import { BEACHS_NE } from "../../constants";
 
-
-const container = () => {
+const Container = () => {
   return (
     <div className={Style.container}>
-        
-        <div className="card">
-            <img src={Porto} alt="" />
+      <div className="card">
+        <img src={Porto} alt="" />
         <div className="card-content">
-            <h1 className='card-title'>Porto de Galinhas</h1>
-            <p className='card-description'>Porto de Galinhas é uma praia localizada no distrito homônimo, pertencente ao município de Ipojuca, no estado de Pernambuco, Brasil. A região possui piscinas de águas claras e mornas formadas entre corais, além de estuários, mangues, areia branca e coqueirais.</p>
-            <button class="botao">Saiba Mais</button>
+          <h1 className="card-title">{BEACHS_NE.PORTO_DE_GALINHAS.NAME}</h1>
+          <p className="card-description">
+            {BEACHS_NE.PORTO_DE_GALINHAS.DESCRIPTION}
+          </p>
+          <button class="botao">Saiba Mais</button>
         </div>
         <div className="buton"></div>
-        </div>
-        <div className="card">
-            <img src={Noronha} alt="" />
+      </div>
+      <div className="card">
+        <img src={Noronha} alt="" />
         <div className="card-content">
-            <h1 className='card-title'>Fernando de Noronha</h1>
-            <p className='card-description'>Fernando de Noronha é um arquipélago vulcânico situado a cerca de 350 quilómetros ao largo da costa nordeste do Brasil. Tem o nome da sua maior ilha, um parque marinho e santuário ecológico protegido com uma linha costeira recortada e vários ecossistemas. É reconhecida pelas suas praias pouco urbanizadas e por atividades como mergulho e snorkeling.</p>
-            <button class="botao">Saiba Mais</button>
+          <h1 className="card-title">{BEACHS_NE.FERNANDO_DE_NORONHA.NAME}</h1>
+          <p className="card-description">
+            {BEACHS_NE.FERNANDO_DE_NORONHA.DESCRIPTION}
+          </p>
+          <button class="botao">Saiba Mais</button>
         </div>
-        </div>
-        <div className="card">
-            <img src={Carneiros} alt="" />
+      </div>
+      <div className="card">
+        <img src={Carneiros} alt="" />
         <div className="card-content">
-            <h1 className='card-title'>Praia dos Carneiros</h1>
-            <p className='card-description'>A Praia dos Carneiros, em Pernambuco, é reconhecida como uma das praias mais bonitas do Brasil e desperta o interesse de muitos viajantes por sua beleza natural. É o lugar perfeito para relaxar e curtir um cenário paradisíaco, cercado por um mar lindo, cheio de piscinas naturais. Estivemos em Carneiros recentemente, por isso vale conferir nossas dicas fresquinhas!</p>
-            <button class="botao">Saiba Mais</button>
+          <h1 className="card-title">{BEACHS_NE.PRAIA_DOS_CARNEIROS.NAME}</h1>
+          <p className="card-description">
+            {BEACHS_NE.PRAIA_DOS_CARNEIROS.DESCRIPTION}
+          </p>
+          <button class="botao">Saiba Mais</button>
         </div>
-        </div>
-        <div className="card">
-            <img src={Enseada} alt="" />
+      </div>
+      <div className="card">
+        <img src={Enseada} alt="" />
         <div className="card-content">
-            <h1 className='card-title'>Enseada dos Corais</h1>
-            <p className='card-description'>A Praia dos Carneiros, em Pernambuco, é reconhecida como uma das praias mais bonitas do Brasil e desperta o interesse de muitos viajantes por sua beleza natural. É o lugar perfeito para relaxar e curtir um cenário paradisíaco, cercado por um mar lindo, cheio de piscinas naturais. Estivemos em Carneiros recentemente, por isso vale conferir nossas dicas fresquinhas!</p>
-            <button class="botao">Saiba Mais</button>
+          <h1 className="card-title">{BEACHS_NE.ENSEADA_DOS_CORAIS.NAME}</h1>
+          <p className="card-description">
+            {BEACHS_NE.ENSEADA_DOS_CORAIS.DESCRIPTION}
+          </p>
+          <button class="botao">Saiba Mais</button>
         </div>
-        </div>
-        <div className="card">
-            <img src={Carneiros} alt="" />
+      </div>
+      <div className="card">
+        <img src={Carneiros} alt="" />
         <div className="card-content">
-            <h1 className='card-title'>Praia dos Carneiros</h1>
-            <p className='card-description'>A Praia dos Carneiros, em Pernambuco, é reconhecida como uma das praias mais bonitas do Brasil e desperta o interesse de muitos viajantes por sua beleza natural. É o lugar perfeito para relaxar e curtir um cenário paradisíaco, cercado por um mar lindo, cheio de piscinas naturais. Estivemos em Carneiros recentemente, por isso vale conferir nossas dicas fresquinhas!</p>
-            <button class="botao">Saiba Mais</button>
+          <h1 className="card-title">{BEACHS_NE.PRAIA_DOS_CARNEIROS.NAME}</h1>
+          <p className="card-description">
+            {BEACHS_NE.PRAIA_DOS_CARNEIROS.DESCRIPTION}
+          </p>
+          <button class="botao">Saiba Mais</button>
         </div>
-        </div>
-        <div className="card">
-            <img src={Carneiros} alt="" />
+      </div>
+      <div className="card">
+        <img src={Carneiros} alt="" />
         <div className="card-content">
-            <h1 className='card-title'>Praia dos Carneiros</h1>
-            <p className='card-description'>A Praia dos Carneiros, em Pernambuco, é reconhecida como uma das praias mais bonitas do Brasil e desperta o interesse de muitos viajantes por sua beleza natural. É o lugar perfeito para relaxar e curtir um cenário paradisíaco, cercado por um mar lindo, cheio de piscinas naturais. Estivemos em Carneiros recentemente, por isso vale conferir nossas dicas fresquinhas!</p>
-            <button class="botao">Saiba Mais</button>
+          <h1 className="card-title">{BEACHS_NE.PRAIA_DOS_CARNEIROS.NAME}</h1>
+          <p className="card-description">
+            {BEACHS_NE.PRAIA_DOS_CARNEIROS.DESCRIPTION}
+          </p>
+          <button class="botao">Saiba Mais</button>
         </div>
-        </div>
-        <div className="card">
-            <img src={Carneiros} alt="" />
+      </div>
+      <div className="card">
+        <img src={Carneiros} alt="" />
         <div className="card-content">
-            <h1 className='card-title'>Praia dos Carneiros</h1>
-            <p className='card-description'>A Praia dos Carneiros, em Pernambuco, é reconhecida como uma das praias mais bonitas do Brasil e desperta o interesse de muitos viajantes por sua beleza natural. É o lugar perfeito para relaxar e curtir um cenário paradisíaco, cercado por um mar lindo, cheio de piscinas naturais. Estivemos em Carneiros recentemente, por isso vale conferir nossas dicas fresquinhas!</p>
-            <button class="botao">Saiba Mais</button>
+          <h1 className="card-title">{BEACHS_NE.PRAIA_DOS_CARNEIROS.NAME}</h1>
+          <p className="card-description">
+            {BEACHS_NE.PRAIA_DOS_CARNEIROS.DESCRIPTION}
+          </p>
+          <button class="botao">Saiba Mais</button>
         </div>
-        </div>
-        
+      </div>
     </div>
-    
+  );
+};
 
-
-  )
-}
-
-export default container
+export default Container;
---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const MENU_ITEMS = {
+  ATTRACTIONS: "Pontos Turísticos",
+  HOME: "Início",
+  CONTATO: "Contato",
+  COASTLINE: "Litoral",
+};
+
+export const BEACHS_NE = {
+  PORTO_DE_GALINHAS: {
+    NAME: "Porto de Galinhas",
+    DESCRIPTION:
+      "Porto de Galinhas é uma praia localizada no distrito homônimo, pertencente ao município de Ipojuca, no estado de Pernambuco, Brasil. A região possui piscinas de águas claras e mornas formadasentre corais, além de estuários, mangues, areia branca e coqueirais.",
+  },
+  FERNANDO_DE_NORONHA: {
+    NAME: "Fernando de Noronha",
+    DESCRIPTION: "Fernando de Noronha é um arquipélago vulcânico situado a cerca de 350 quilómetros ao largo da costa nordeste do Brasil. Tem o nome da sua maior ilha, um parque marinho e santuário ecológico protegido com uma linha costeira recortada e vários ecossistemas. É reconhecida pelas suas praias pouco urbanizadas e por atividades como mergulho e snorkeling."
+  },
+  PRAIA_DOS_CARNEIROS: {
+    NAME: "Praia dos Carneiros",
+    DESCRIPTION: "A Praia dos Carneiros, em Pernambuco, é reconhecida como uma das praias mais bonitas do Brasil e desperta o interesse de muitos viajantes por sua beleza natural. É o lugar perfeito para relaxar e curtir um cenário paradisíaco, cercado por um mar lindo, cheio de piscinas naturais. Estivemos em Carneiros recentemente, por isso vale conferir nossas dicas fresquinhas!"
+  },
+  ENSEADA_DOS_CORAIS: {
+    NAME: "Ensada dos Corais",
+    DESCRIPTION: " Essa praia é muito frequentada pelos surfistas pois ela possui uma ondulação acima da média e aí você sempre vai encontrar a galera do surfe “pegando ondas”.. Reza a lenda que batizaram esse trecho da praia com “PQP” (expressão de baixo calão da língua portuguesa) porque normalmente as ondulações são muito traiçoeiras levando o surfista desavisado ao encontro das pedras."
+  }
+};
